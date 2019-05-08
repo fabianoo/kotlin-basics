7ba2778b57c31d7ee5b4c940e1fb8ef70f81cb3a
Fabiano R Oliveira <eu@fabiano.xyz>
2019-05-08 09:53:37 -0300

    Adding String syntaxes

--- src/main/kotlin/xyz/fabiano/kotlin/2_Strings.kt
+++ src/main/kotlin/xyz/fabiano/kotlin/2_Strings.kt
@@ -1,41 +1,44 @@
 package xyz.fabiano.kotlin
 
-/**
- * O ponto de partida de um programa Kotlin, assim como no Java, é uma função pública main.
- *  Embora a assinatura do método seja diferente, elas são equivalentes.
- *
- *  Note que essa função não é estática. No Kotlin não existe o conceito de função estática
- *
- *  O valor de args, assim como no Java, são os valores passados como argumentos na linha de comando.
- *
- *  Desde o Kotlin 1.3, temos a opção de receber nenhum argumento, simplesmente "fun main() {}"
- */
-fun main(args: Array<String>) {
+fun main() {
 
     /*
-        Semelhante ao Java, temos comentários  com '//' ou '/**/'
-     */
-    // A função que imprime na saída padrão é semelhante, embora mais simples
-    println("Hello World!")
-
-    /*
-        Podemos declarar variáveis com as palavras "var" ou "val":
+        Strings são representadas como no Java. Escapamos Strings da mesma forma, com backslack "\"
     */
-    val value = 10 // we cannot later reassign fooVal to something else
-    var variavel = 10
-
-    variavel = 20 // Podemos atribuir outro valor para um "var"
-    /*
-        Enquanto uma variável declarada como "var" pode ter outro valor atribuído, uma declarada com "val" não pode
-            - "val" segue os princípios de imutabilidade tão fundamentais para a programação funcional.
-            - Vale sempre dar preferência à declarações com "val" quando possível.
-     */
+    val myString = "String simplona!"
+    val newLineString = "Vamos imprimir uma nova linha?\nAqui está!"
+    println(myString)
+    println(newLineString)
 
 
     /*
-        Na maioria dos casos, o Kotlin consegue determinar qual o tipo da sua variável sem declaração explícita.
-        Portanto, valA e valB são equivalentes:
+        As diferenças começam aqui. No Kotlin temos Strings de muitas linhas e formatadas. Estas são delimitadas por """:
     */
-    val valA: Int = 7
-    val valB = 7
+    val rawString = """
+        fun helloWorld(val name : String) {
+           println("Hello, world!")
+        }
+        """
+    println(rawString)
+
+    /*
+        Podemos cortar a identação da String de maneira muito simples:
+     */
+    val rawStringSemIdentacao = """
+        fun helloWorld(val name : String) {
+           println("Hello, world!")
+        }
+        """.trimIndent()
+
+    println(rawStringSemIdentacao)
+
+    /*
+        Concatenação de Strings no Kotlin pode ser muito mais bonito ;)
+        Para isso, podemos usar o template expression, que começam com "$"
+
+        Vale notar as 2 formas: Com e sem chaves "{}".
+        Com chaves, podemos atribuir uma expressão de valor, por exemplo, uma função
+    */
+    val templateString = "$myString has ${myString.length} characters"
+    println(templateString)
 }
